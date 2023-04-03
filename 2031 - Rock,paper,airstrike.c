29df8f15c3d246d94f86744f4c30839105eba94d
Preetom Aditya Pranoy <129541981+wizard-pranoy@users.noreply.github.com>
2023-04-04 00:18:28 +0600

    Update 2031 - Rock,paper,airstrike.c

--- 2031 - Rock,paper,airstrike.c
+++ 2031 - Rock,paper,airstrike.c
@@ -1,41 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     int n,i=0;
 
+    char a[14];
+    char b[14];
     scanf(" %d", &n);
 
 
     while (i<n)
     {
-        char a[7];
-        char b[7];
 
-            scanf(" %s", a);
-            scanf(" %s", b);
+
+        scanf(" %s", a);
+        scanf(" %s", b);
+        getchar();
 
         // conditions for outputs
-        // 'a' represents airstrike, 'p' represents rock
-        // 'a[1] = a' represents paper
+        // 't' represents airstrike, 'e' represents rock
+        // 'a' represents paper
 
-        if ( a[0] == 'a' && ( b[0]== 'p' || b[1] == 'a') )
+        if ( a[1]=='t' && ( b[1]=='e' || b[1]=='a' ) )
         {
             printf("Jogador 1 venceu\n");
         }
-        else if ( a[0] == 'p' && b[1] == 'a')
+        else if ( a[1] == 'e' && b[1] == 'a' )
         {
             printf("Jogador 1 venceu\n");
         }
-        else if ( a[1] == 'a' && b[1] == 'a' )
+        else if ( a[1] == 'a' && b[1] == 'a')
         {
             printf("Ambos venceram\n");
         }
-        else if ( a[0] == 'p' && b[0] == 'p' )
+        else if ( a[1] == 'e' && b[1] == 'e' )
         {
             printf("Sem ganhador\n");
         }
-         else if ( a[0] == 'a' && b[0] == 'a' )
+        else if ( a[1] == 't' && b[1] == 't' )
         {
             printf("Aniquilacao mutua\n");
         }
@@ -43,7 +46,6 @@ int main()
         {
             printf("Jogador 2 venceu\n");
         }
-
 
         i++;
     }
